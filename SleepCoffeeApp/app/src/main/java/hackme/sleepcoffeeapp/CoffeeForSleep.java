3a1cf666ec439912bce173e9b844ff762e5be926
pyerrami <pawan.yerramilli@outlook.com>
2019-03-16 09:11:43 -0400

    Created Activities

---
+++ SleepCoffeeApp/app/src/main/java/hackme/sleepcoffeeapp/CoffeeForSleep.java
@@ -0,0 +1,13 @@
+package hackme.sleepcoffeeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoffeeForSleep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coffee_for_sleep);
+    }
+}
