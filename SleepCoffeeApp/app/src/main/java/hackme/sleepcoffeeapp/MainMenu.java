76199402d7f753faedba3fb8993fab092e74bca6
pyerrami <pawan.yerramilli@outlook.com>
2019-03-16 09:29:28 -0400

    Added intent

--- SleepCoffeeApp/app/src/main/java/hackme/sleepcoffeeapp/MainMenu.java
+++ SleepCoffeeApp/app/src/main/java/hackme/sleepcoffeeapp/MainMenu.java
@@ -1,7 +1,10 @@
 package hackme.sleepcoffeeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,4 +13,24 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
+
+
+
+
+    public void OnClickForSleep(View v) {
+        Intent ForSleep = new Intent(this, CoffeeForSleep.class);
+    }
+
+    public void OnClickEnterCoffee(View v) {
+        Intent ForSleep = new Intent(this, CoffeeForSleep.class);
+    }
+
+    public void OnClickTillSleep(View v) {
+        Intent ForSleep = new Intent(this, CoffeeForSleep.class);
+    }
+
+    public void OnClickSignIn(View v) {
+        Intent ForSleep = new Intent(this, CoffeeForSleep.class);
+    }
+
 }
